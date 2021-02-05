f629d8ee03d036823d8b1a6e5e66a7fca9706a47
son <asr1203@gmail.com>
2021-02-05 09:49:26 +0900

    자바 옵셔널 관련
    언젠가 알아두면 사용하겠지

---
+++ Etc/javaOptional.java
@@ -0,0 +1,10 @@
+java optional  적용법
+
+Map<Integer, String> shopName = new HashMap<>();
+shopName.put(1, "11st");
+shopName.put(2, "쿠팡");
+
+
+Optional<String> c = Optional.ofNullable(shopName.get(4)); // 기존이였다면 에러 호출 되는 시점
+int length = shopName.map(String::length).orElse(0); //
+System.out.println("length: " + length);
